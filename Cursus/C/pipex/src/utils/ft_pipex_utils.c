c9cfc9804b4e7defe68d451f04647dafe12f456c
Adrián Escribano Pérez <adrian.escribano3@gmail.com>
2025-08-17 17:51:24 +0200

    Comentario de los archivos de lectura

--- Cursus/C/pipex/src/utils/ft_pipex_utils.c
+++ Cursus/C/pipex/src/utils/ft_pipex_utils.c
@@ -6,12 +6,33 @@
 /*   By: adriescr <adriescr@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 18:14:27 by adriescr          #+#    #+#             */
-/*   Updated: 2025/08/17 17:44:24 by adriescr         ###   ########.fr       */
+/*   Updated: 2025/08/17 17:51:03 by adriescr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../pipex.h"
 
+/*
+ * ENGLISH: Opens an input file with the specified name.
+ *
+ * SPANISH: Abre un archivo de entrada con el nombre especificado.
+ *
+ * @param filename   
+ * 					The name of the file to open. / 
+ * 					El nombre del archivo a abrir.
+ * @param outfile     
+ * 					File descriptor for the output file. / 
+ * 					Descriptor de archivo para el archivo de salida.
+ * @param outfile_error
+ * 					Error status for output file opening. / 
+ * 					Estado de error al abrir el archivo de salida.
+ *
+ * @return {int}
+ * 
+ * - Returns the file descriptor if successful, or -1 on error.
+ * 
+ * - Devuelve el descriptor de archivo si tiene éxito, o -1 en caso de error.
+ */
 void	child1_process(t_pipex_ctx *ctx)
 {
 	int	devnull;
@@ -33,6 +54,27 @@ void	child1_process(t_pipex_ctx *ctx)
 	ft_exec_cmd(ctx->argv[2], ctx->envp);
 }
 
+/*
+ * ENGLISH: Opens an output file with the specified name.
+ *
+ * SPANISH: Abre un archivo de salida con el nombre especificado.
+ *
+ * @param filename   
+ * 					The name of the file to open. / 
+ * 					El nombre del archivo a abrir.
+ * @param infile     
+ * 					File descriptor for the input file. / 
+ * 					Descriptor de archivo para el archivo de entrada.
+ * @param infile_error
+ * 					Error status for input file opening. / 
+ * 					Estado de error al abrir el archivo de entrada.
+ *
+ * @return {int}
+ * 
+ * - Returns the file descriptor if successful, or -1 on error.
+ * 
+ * - Devuelve el descriptor de archivo si tiene éxito, o -1 en caso de error.
+ */
 void	child2_process(t_pipex_ctx *ctx)
 {
 	dup2(ctx->fd[0], 0);
